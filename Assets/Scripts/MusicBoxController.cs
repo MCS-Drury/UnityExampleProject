c68478d35edf535f70412b2b33f0b64f70054c0c
SarahSalad <selester12@gmail.com>
2020-11-19 19:13:47 -0600

    Added music box script/audio

---
+++ Assets/Scripts/MusicBoxController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBoxController : MonoBehaviour
+{
+    public AudioClip music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = music;
+    }
+
+    //Play sound whenever collision is detected 
+    void OnCollisionEnter()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
