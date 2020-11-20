c47a00eb968d02bb8532693df478fb04e31be3c5
SarahSalad <selester12@gmail.com>
2020-11-19 20:04:21 -0600

    Added trigger to turn music box on and off

--- Assets/Scripts/MusicBoxController.cs
+++ Assets/Scripts/MusicBoxController.cs
@@ -5,17 +5,47 @@ using UnityEngine;
 public class MusicBoxController : MonoBehaviour
 {
     public AudioClip music;
+    private bool isMusicPlaying;
+    private bool inTrigger;
+    public Canvas triggerCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<AudioSource>().playOnAwake = false;
         GetComponent<AudioSource>().clip = music;
+        isMusicPlaying = false;
+        inTrigger = false;
     }
 
     //Play sound whenever collision is detected 
-    void OnCollisionEnter()
+    void OnTriggerEnter2D(Collider2D col)
     {
-        GetComponent<AudioSource>().Play();
+        inTrigger = true;
+        triggerCanvas.enabled = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        inTrigger = false;
+        triggerCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && inTrigger)
+        {
+            Debug.Log("here");
+            if (!isMusicPlaying)
+            {
+                GetComponent<AudioSource>().Play();
+                isMusicPlaying = true;
+            }
+            else
+            {
+                GetComponent<AudioSource>().Stop();
+                isMusicPlaying = false;
+            }
+        }
     }
 }
